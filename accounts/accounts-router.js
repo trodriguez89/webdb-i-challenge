c6072f791252d356d8dd7cea8807acc082ed9f67
trodriguez89 <trodriguez89@gmail.com>
2020-01-21 12:46:07 -0800

    initial commit, GET POST PUT working. Forgot to commit earlier sorry. Mvp wip

---
+++ accounts/accounts-router.js
@@ -0,0 +1,60 @@
+const express = require("express");
+
+const db = require("../data/dbConfig");
+
+const router = express.Router();
+
+// GET requests
+router.get("/", (req, res) => {
+    db.select('*').from('accounts')
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({errorMessage: "something went wrong!"})
+    })
+});
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    db.select('*').from('accounts').where('id', '=', id)
+    .first()
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(error => {
+        res.status(500).json({errorMessage: "something went wrong!"})
+    })
+});
+
+// POST request
+router.post("/", (req, res) => {
+    db.insert(req.body, 'id').into('accounts')
+    .then(newId => {
+        res.status(200).json(newId)
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({errorMessage: "something went wrong!"})
+    })
+});
+
+// PUT requests
+router.put("/:id", (req, res) => {
+    const updateId = req.params.id;
+    const body = req.body;
+    db('accounts').where({ id: updateId })
+    .update(body)
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({errorMessage: "something went wrong!"})
+    })
+});
+
+module.exports = router;
+
+
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+// const db = require('./data/dbConfig.js');
+
+const accountRouter =  require("./accounts/accounts-router");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", accountRouter);
 
 module.exports = server;
